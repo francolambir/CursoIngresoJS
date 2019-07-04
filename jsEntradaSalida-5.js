c0a2e840c8ec3c4f80f6c45d623036aca55fc033
francolambir <52534596+francolambir@users.noreply.github.com>
2019-07-04 12:14:04 -0300

    ejercicios 2

--- jsEntradaSalida-5.js
+++ jsEntradaSalida-5.js
@@ -1,7 +1,14 @@
-/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
-{	
-	
+{	var nombre;
+  var edad;
+
+nombre = document.getElementById("elNombre").value = dato ;
+edad = document.getElementById("laEdad").value = dato;
+alert("susNombres" + nombre + "su edad es" + edad);
+
+
+
 }
 
