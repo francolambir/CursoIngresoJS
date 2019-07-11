e29fa25c17e33351973fa5fa4408cad1efa1d95a
francolambir <52534596+francolambir@users.noreply.github.com>
2019-07-11 12:19:58 -0300

    ejercicios

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-//tomo la edad  
-
-
+//tomo la edad
+var edad;
+edad = document.getElementById("edad").value;
+if (edad >= 18) {
+  alert ("Es mayor de edad");
+} else if (edad <= 17); {
+  alert ("Es menor de edad");
+}
 }//FIN DE LA FUNCIÓN
